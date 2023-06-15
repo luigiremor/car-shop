5b23ec2488bc13b27b6cd1c39a7dd6f4fd509ac0
Luigi Remor <luigiremor@gmail.com>
2023-06-15 13:24:56 -0300

    refactor: list box to tree view

--- view/car_shop_view.py
+++ view/car_shop_view.py
@@ -48,8 +48,21 @@ class CarView(tk.Tk):
             self.principal_view, text="Add Car", command=self.add_car)
         add_button.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
 
-        self.car_listbox = tk.Listbox(self)
-        self.car_listbox.pack(padx=10, pady=10)
+        self.car_treeview = ttk.Treeview(self, columns=(
+            "brand", "model", "year", "price", "status"))
+        self.car_treeview.pack()
+
+        self.car_treeview.heading("brand", text="Brand")
+        self.car_treeview.heading("model", text="Model")
+        self.car_treeview.heading("year", text="Year")
+        self.car_treeview.heading("price", text="Price")
+        self.car_treeview.heading("status", text="Status")
+
+        self.car_treeview.column("brand", width=100)
+        self.car_treeview.column("model", width=100)
+        self.car_treeview.column("year", width=50)
+        self.car_treeview.column("price", width=80)
+        self.car_treeview.column("status", width=80)
 
         filter_label = tk.Label(self, text="Filter by brand:")
         filter_label.pack(padx=10, pady=10)
@@ -73,9 +86,8 @@ class CarView(tk.Tk):
         self.get_all_cars()
 
     def filter_by_brand(self):
-        self.car_listbox.delete(0, tk.END)
+        self.car_treeview.delete(*self.car_treeview.get_children())
         brand = self.filter_entry.get()
-        print(brand, 'filter by brand')
         if brand == "":
             return self.get_all_cars()
 
@@ -83,16 +95,15 @@ class CarView(tk.Tk):
         self.show_cars(cars)
 
     def get_all_cars(self):
-        self.car_listbox.delete(0, tk.END)
+        self.car_treeview.delete(*self.car_treeview.get_children())
         cars = self.controller.get_all_cars()
         for car in cars:
-            self.car_listbox.insert(tk.END, car)
+            self.car_treeview.insert("", tk.END, values=car)
 
     def show_message(self, message):
         messagebox.showinfo("Message", message)
 
     def show_cars(self, cars):
-        self.car_listbox.delete(0, tk.END)
+        self.car_treeview.delete(*self.car_treeview.get_children())
         for car in cars:
-            print(car, 'show cars')
-            self.car_listbox.insert(tk.END, car)
+            self.car_treeview.insert("", tk.END, values=car)
